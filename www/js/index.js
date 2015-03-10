71faaccac29ed6937908127bec6ba37a7a5a77e1
johannluthi <johann.luthi@gmail.com>
2015-03-10 15:47:56 +0100

    clean

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -47,11 +29,12 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-
+	
+// Object php API
 
 function load(objet) {
 	$('#list').show();
-    $.ajax("api/" + objet + ".php").done(function(data) {
+    $.ajax("http://www.educh.ch/api/" + objet + ".php").done(function(data) {
         var i, obj;
 		$("#all").empty();
         $.each(data.items, function (i, obj) {
@@ -64,7 +47,7 @@ function load(objet) {
 }
 function loadObjet(objet,id) {
 	$('#objet').show();
-    $.ajax("api/o" + objet + ".php?id=" + id).done(function(data) {
+    $.ajax("http://www.educh.ch/api/o" + objet + ".php?id=" + id + ":3000").done(function(data) {
 		$("#objet").empty();
         $("#objet").append("<h2>" + data.item.title + "</h2><a href='http://www.educh.ch/" + data.item.url + "' data-role='button' data-inline='true' class='btn'>"+data.item.label_link+"</a><p>" + data.item.texte + "</p>");
         $('#objet').listview('refresh');
