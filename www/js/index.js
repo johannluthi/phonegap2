7a2f02d1479af636790345b2835708b347430b07
johannluthi <johann.luthi@gmail.com>
2015-03-10 18:30:30 +0100

    design

--- www/js/index.js
+++ www/js/index.js
@@ -19,6 +19,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+		
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -27,7 +28,7 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
 };
 	
 // Object php API
